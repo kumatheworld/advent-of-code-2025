4e131d432acbf7ef20ecdd18713bcb80a9240f3f
kumatheworld <kum4th3w0rld@gmail.com>
2025-12-06 12:45:26 +0000

    Day 3 part 2 done

--- src/Day03.hs
+++ src/Day03.hs
@@ -1,25 +1,30 @@
 module Day03 (solution, part1, part2, tests) where
 
 import AoC.Template (Day (..), readExample, solve)
-import Data.List (elemIndex)
 import qualified Data.Text as T
 import Test.Hspec
 
 day :: Day
 day = Day 3
 
-maxJoltage :: String -> Maybe Int
-maxJoltage s = do
-  let mx = maximum s
-  argmax <- elemIndex mx s
-  let (c, d) = if argmax == length s - 1 then (maximum $ init s, mx) else (mx, maximum $ drop (argmax + 1) s)
-  return $ read [c, d]
+maxJoltage' :: [String] -> String -> Int
+maxJoltage' acc "" = read $ head acc
+maxJoltage' acc (c : s) = maxJoltage' acc' s
+  where
+    acc' = snd $ foldr step ("", []) acc
+    step curr (prev, accTail) = (curr, max curr (prev ++ [c]) : accTail)
+
+maxJoltage :: Int -> String -> Int
+maxJoltage n = maxJoltage' (replicate n "")
+
+part :: Int -> T.Text -> Maybe Int
+part n = Just . sum . map (maxJoltage n . T.unpack) . T.lines
 
 part1 :: T.Text -> Maybe Int
-part1 = fmap sum . mapM (maxJoltage . T.unpack) . T.lines
+part1 = part 2
 
 part2 :: T.Text -> Maybe Int
-part2 _ = Nothing
+part2 = part 12
 
 solution :: IO ()
 solution = solve day part1 part2
