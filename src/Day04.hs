552556d4572ea95591023c61999eb2c01539ac69
kumatheworld <kum4th3w0rld@gmail.com>
2025-12-06 15:51:33 +0000

    Day 4 part 1 done

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,14 +1,30 @@
 module Day04 (solution, part1, part2, tests) where
 
+import AoC.Lib (readMatrix)
 import AoC.Template (Day (..), readExample, solve)
+import Data.Matrix
 import qualified Data.Text as T
 import Test.Hspec
 
 day :: Day
 day = Day 4
 
+neighbors :: (Int, Int) -> Matrix a -> [Maybe a]
+neighbors ij mat =
+  [ safeGet i' j' mat
+    | di <- [-1 .. 1],
+      dj <- [-1 .. 1],
+      (di, dj) /= (0, 0),
+      let i' = fst ij + di,
+      let j' = snd ij + dj
+  ]
+
 part1 :: T.Text -> Maybe Int
-part1 _ = Nothing
+part1 input = Just . length $ filter accessible ijs
+  where
+    accessible ij = mat ! ij == '@' && length (filter (== Just '@') (neighbors ij mat)) < 4
+    ijs = [(i, j) | i <- [1 .. nrows mat], j <- [1 .. ncols mat]]
+    mat = readMatrix input
 
 part2 :: T.Text -> Maybe Int
 part2 _ = Nothing
