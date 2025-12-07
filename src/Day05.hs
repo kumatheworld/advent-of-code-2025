e3b25bf1b5649a6b446a008acc3ac52dca9aa9fc
kumatheworld <kum4th3w0rld@gmail.com>
2025-12-07 10:16:10 +0000

    Prepare for day 5 part 2

--- src/Day05.hs
+++ src/Day05.hs
@@ -31,11 +31,6 @@ tests = describe "Day05" $ do
     input <- readExample day 1
     part1 input `shouldBe` Just 3
 
-  -- Uncomment to test with additional examples (create files like 05-2.txt, 05-3.txt, etc.)
-  -- it "solves part 1 with example 2" $ do
-  --   input <- readExample day 2
-  --   part1 input `shouldBe` Just 0
-
   it "solves part 2 with example 1" $ do
     input <- readExample day 1
-    part2 input `shouldBe` Just 0 -- TODO: Replace with expected value
+    part2 input `shouldBe` Just 14
