ee75c1f12ecdcd77a94e7a73ff54ece451d2519d
kumatheworld <kum4th3w0rld@gmail.com>
2025-12-07 11:24:18 +0000

    Day 5 part 2 done

--- src/Day05.hs
+++ src/Day05.hs
@@ -1,25 +1,37 @@
-{-# LANGUAGE TypeApplications #-}
-
 module Day05 (solution, part1, part2, tests) where
 
 import AoC.Lib (readNum, splitToPair)
 import AoC.Template (Day (..), readExample, solve)
+import Data.Bifunctor
+import qualified Data.List as L
 import qualified Data.Text as T
 import Test.Hspec
 
 day :: Day
 day = Day 5
 
+part :: T.Text -> ([(Int, Int)], T.Text)
+part input =
+  let (rangesT, idsT) = splitToPair "\n\n" input
+      rangesMessed = map (bimap readNum readNum . splitToPair "-") $ T.split (== '\n') rangesT
+      ranges = foldr step [] $ L.sortOn (\(x, y) -> (y, x)) rangesMessed
+   in (ranges, idsT)
+  where
+    step xy [] = [xy]
+    step xy@(x, y) xys@((x', y') : xys')
+      | x >= x' = xys
+      | y < x' - 1 = xy : xys
+      | otherwise = (x, y') : xys'
+
 part1 :: T.Text -> Maybe Int
 part1 input =
-  let (rangesS, idsS) = splitToPair "\n\n" input
-      ranges = map (splitToPair "-") $ T.split (== '\n') rangesS
-      ids = map readNum . filter (/= T.pack "") $ T.split (== '\n') idsS
-      isFresh n = or $ [readNum @Int lt <= n && n <= readNum @Int rt | (lt, rt) <- ranges]
+  let (ranges, idsT) = part input
+      ids = map readNum . filter (/= T.pack "") $ T.split (== '\n') idsT
+      isFresh n = or $ [l <= n && n <= r | (l, r) <- ranges] -- binary search would be faster but who cares
    in Just . length . filter isFresh $ ids
 
 part2 :: T.Text -> Maybe Int
-part2 _ = Nothing
+part2 = Just . sum . map (\(l, r) -> r - l + 1) . fst . part
 
 solution :: IO ()
 solution = solve day part1 part2
