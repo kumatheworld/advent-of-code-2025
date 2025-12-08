fff7f03061ad707a9699d4c2da70c7c28ee9618d
kumatheworld <kum4th3w0rld@gmail.com>
2025-12-08 21:29:44 +0000

    Day 8 part 1 done

--- src/Day08.hs
+++ src/Day08.hs
@@ -1,14 +1,27 @@
+{-# LANGUAGE TypeApplications #-}
+
 module Day08 (solution, part1, part2, tests) where
 
+import AoC.Lib (readNum)
 import AoC.Template (Day (..), readExample, solve)
+import Data.Graph
+import qualified Data.List as L
 import qualified Data.Text as T
+import Data.Tree
 import Test.Hspec
 
 day :: Day
 day = Day 8
 
 part1 :: T.Text -> Maybe Int
-part1 _ = Nothing
+part1 input =
+  let vs = zip [1 ..] . map (map (readNum @Int) . T.split (== ',')) . T.lines $ input
+      ds = [((m, n), dist2 xs ys) | (m, xs) <- vs, (n, ys) <- vs, m < n]
+      es = take (if length vs < 100 then 10 else 1000) . map fst . L.sortOn snd $ ds
+      cs = L.sortOn (negate . length) . components $ buildG (1, length vs) es
+   in Just . product . take 3 . map (length . flatten) $ cs
+  where
+    dist2 xs ys = foldr (\(x, y) -> ((x - y) * (x - y) +)) 0 $ zip xs ys
 
 part2 :: T.Text -> Maybe Int
 part2 _ = Nothing
