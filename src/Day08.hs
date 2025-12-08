55b0ace3f4fca03f759bfd9639e331f5e6cb1a12
kumatheworld <kum4th3w0rld@gmail.com>
2025-12-08 23:08:14 +0000

    Day 8 part 2 done

--- src/Day08.hs
+++ src/Day08.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeApplications #-}
 
 module Day08 (solution, part1, part2, tests) where
 
 import AoC.Lib (readNum)
 import AoC.Template (Day (..), readExample, solve)
+import Control.Monad
+import Data.Equivalence.Monad
 import Data.Graph
 import qualified Data.List as L
 import qualified Data.Text as T
@@ -13,18 +16,36 @@ import Test.Hspec
 day :: Day
 day = Day 8
 
+part :: T.Text -> ([Int], [(Int, Int)])
+part input =
+  let vs = zip [0 ..] . map (map (readNum @Int) . T.split (== ',')) . T.lines $ input
+      xs = map (head . snd) vs
+      es = map fst $ L.sortOn snd [((m, n), dist2 p q) | (m, p) <- vs, (n, q) <- vs, m < n]
+   in (xs, es)
+  where
+    dist2 p q = foldr (\(x, y) -> ((x - y) * (x - y) +)) 0 $ zip p q
+
 part1 :: T.Text -> Maybe Int
 part1 input =
-  let vs = zip [1 ..] . map (map (readNum @Int) . T.split (== ',')) . T.lines $ input
-      ds = [((m, n), dist2 xs ys) | (m, xs) <- vs, (n, ys) <- vs, m < n]
-      es = take (if length vs < 100 then 10 else 1000) . map fst . L.sortOn snd $ ds
-      cs = L.sortOn (negate . length) . components $ buildG (1, length vs) es
-   in Just . product . take 3 . map (length . flatten) $ cs
-  where
-    dist2 xs ys = foldr (\(x, y) -> ((x - y) * (x - y) +)) 0 $ zip xs ys
+  let (xs, es) = part input
+      n = length xs
+      cs = components . buildG (0, n - 1) . take (if n < 100 then 10 else 1000) $ es
+   in Just . product . take 3 . map (length . flatten) . L.sortOn (negate . length) $ cs
 
+-- The credit goes to https://stackoverflow.com/a/4293437
+-- I have no idea why it's working
 part2 :: T.Text -> Maybe Int
-part2 _ = Nothing
+part2 input =
+  let (xs, es) = part input
+      (i, j) = last $ kruskal es
+   in Just $ xs !! i * xs !! j
+  where
+    run = runEquivM (const ()) (const $ const ())
+    kruskal es = run $ filterM go es
+      where
+        go (u, v) = do
+          eq <- equivalent u v
+          if eq then return False else equate u v >> return True
 
 solution :: IO ()
 solution = solve day part1 part2
