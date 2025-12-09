52fd28344228177ccb2cbce4602a7801bb4ccf25
kumatheworld <kum4th3w0rld@gmail.com>
2025-12-09 22:19:20 +0000

    Day 9 part 2 kind of done

--- src/Day09.hs
+++ src/Day09.hs
@@ -9,14 +9,40 @@ import Test.Hspec
 day :: Day
 day = Day 9
 
-part1 :: T.Text -> Maybe Int
-part1 input =
+part :: T.Text -> ([(Int, Int)], [(((Int, Int), (Int, Int)), Int)])
+part input =
   let ps = map (bimap readNum readNum . splitToPair ",") $ T.lines input
-      areas = [(x2 - x1 + 1) * abs (y2 - y1 + 1) | p1@(x1, y1) <- ps, p2@(x2, y2) <- ps, p1 < p2]
-   in Just $ maximum areas
+      rects = [((p1, p2), (x2 - x1 + 1) * (abs (y2 - y1) + 1)) | p1@(x1, y1) <- ps, p2@(x2, y2) <- ps, p1 < p2]
+   in (ps, rects)
 
+part1 :: T.Text -> Maybe Int
+part1 = Just . maximum . map snd . snd . part
+
+-- The part 2 solution is technically wrong but works for the specific test and input
+-- Counterexample:
+-- #.....#
+-- .......
+-- #...#..
+-- .......
+-- .......
+-- .......
+-- ....#.#
+-- the algorithm would find the bottom left rectangle of area 25 right
 part2 :: T.Text -> Maybe Int
-part2 _ = Nothing
+part2 input =
+  let (ps, rects) = part input
+      es = zip ps (tail ps ++ [head ps])
+   in Just . maximum . map snd . filter (\(pp, _) -> all (doesNotIntersect pp) es) $ rects
+  where
+    doesNotIntersect ((x1, y1), (x2, y2)) ((u1, v1), (u2, v2)) -- rectangle and edge respectively
+      | u1 == u2 = u1 <= x1 || x2 <= u1 || v2' <= y1' || y2' <= v1'
+      | v1 == v2 = v1 <= y1' || y2' <= v1 || u2' <= x1 || x2 <= u1'
+      | otherwise = undefined
+      where
+        minmax z w = if z < w then (z, w) else (w, z)
+        (y1', y2') = minmax y1 y2
+        (u1', u2') = minmax u1 u2
+        (v1', v2') = minmax v1 v2
 
 solution :: IO ()
 solution = solve day part1 part2
