d79c87c89496654815bf110dc6ce33444b7769d1
kumatheworld <kum4th3w0rld@gmail.com>
2025-12-10 12:35:37 +0000

    Day 10 part 1 done

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,14 +1,28 @@
 module Day10 (solution, part1, part2, tests) where
 
+import AoC.Lib (readNum)
 import AoC.Template (Day (..), readExample, solve)
+import Data.Bits
+import qualified Data.List as L
 import qualified Data.Text as T
 import Test.Hspec
+import Text.Regex.TDFA
 
 day :: Day
 day = Day 10
 
 part1 :: T.Text -> Maybe Int
-part1 _ = Nothing
+part1 input =
+  let pattern = "\\[(.*)\\] \\((.*)\\) \\{(.*)\\}"
+      toBits :: [Int] -> Int
+      toBits = foldl (.|.) 0 . map bit
+      fewests = do
+        line <- lines . T.unpack $ input
+        let (_, _, _, [lightsS, togglesS, _]) = line =~ pattern :: (String, String, String, [String])
+        let lights = toBits . L.elemIndices '#' $ lightsS
+        let toggles = map (toBits . map readNum . T.split (== ',')) . T.splitOn (T.pack ") (") . T.pack $ togglesS
+        return . length . head . filter ((== lights) . foldl xor 0) . L.sortOn length $ L.subsequences toggles
+   in Just $ sum fewests
 
 part2 :: T.Text -> Maybe Int
 part2 _ = Nothing
